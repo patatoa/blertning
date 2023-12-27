1bc433144c87ccccc971e72e497210c74f75cc90
patatoa <patatoa@gmail.com>
2023-12-27 04:58:03 +0000

    Add SQLite database functionality

--- main.cpp
+++ main.cpp
@@ -3,14 +3,11 @@
 #include <vector>
 #include <fstream>
 #include <openssl/md5.h>
-#include <ctime> // Add this line
+#include <ctime> 
 #include <chrono> 
+#include <sqlite3.h>
 using namespace std;
 
-// a struct that represents file information:
-// - filename
-// - file contents hash
-// - last modified date
 struct FileInfo
 {
 	string filename;
@@ -20,13 +17,15 @@ struct FileInfo
 
 string getFileContentsHash(const string& filePath);
 string convertFileTimeToString(const filesystem::file_time_type& fileTime);
+void saveToDatabase(const vector<FileInfo>& files);
+void createSqliteDatabase();
 
 vector<FileInfo> listFiles()
 {
 	vector<FileInfo> files;
 	for (auto& entry : filesystem::recursive_directory_iterator("."))
 	{
-		if (entry.is_regular_file()) {
+		if (!entry.is_regular_file()) {
             continue; // Skip directories
         }
 		FileInfo info;
@@ -80,9 +79,73 @@ string convertFileTimeToString(const filesystem::file_time_type& fileTime)
 
 	return lastModifiedTimeString;
 }
+
+void saveToDatabase(const vector<FileInfo>& files)
+{
+    sqlite3* db;
+    char* zErrMsg = 0;
+    int rc;
+	bool dbConnected = false;
+
+	while(!dbConnected)
+	{
+		// Open database
+		rc = sqlite3_open("test.db", &db);
+
+		if (rc) {
+			fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+			return;
+		}
+		else
+		{
+			dbConnected = true;
+		}
+	}
+    // Open database
+    rc = sqlite3_open("test.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return;
+    }
+
+    // Create SQL statement to check if table exists
+    char* sql = "CREATE TABLE IF NOT EXISTS FileInfo("  \
+                "Filename TEXT PRIMARY KEY NOT NULL," \
+                "LastModifiedDate TEXT NOT NULL," \
+                "Hash TEXT NOT NULL);";
+
+    // Execute SQL statement
+	auto callback = [](void* NotUsed, int argc, char** argv, char** azColName) -> int { return 0; };
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Table created successfully\n");
+    }
+
+    // Insert FileInfo results into the table
+    for (const auto& file : files) {
+        std::string sql = "INSERT INTO FileInfo (Filename,LastModifiedDate,Hash) " \
+                          "VALUES ('" + file.filename + "', '" + file.lastModifiedDate + "', '" + file.hash + "');";
+        rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        } else {
+            fprintf(stdout, "Records created successfully\n");
+        }
+    }
+
+    sqlite3_close(db);
+}
 int main()
 {
 	vector<FileInfo> files = listFiles();
+	saveToDatabase(files);
 	for (auto& file : files)
 	{
 		cout << file.filename << " " << file.lastModifiedDate << endl;
